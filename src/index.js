0e506166c77a8a35631a0b3f75a25fa88ae22c0a
aigcoding <aig.coding@gmail.com>
2021-03-11 21:45:06 +0300

    feat: implement func min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined || array.length == 0) {
+                  return 0;} 
+  else {
+  m = array[0];
+  for (i = 1; i < array.length; i++) {
+if (array[i] < m) {
+        m = array[i]} 
+}
+return m;
 }
 
-exports.max = function max (array) {
-  return 0;
+}
+
+exports.max = function max(array) {
+  if (array == undefined || array.length == 0) {
+                  return 0;} 
+  else {
+  m = array[0];
+  for (i = 1; i < array.length; i++) {
+if (array[i] > m) {
+        m = array[i]} 
+}
+return m;
+}
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+                  return 0;} 
+  else {
+  a = 0;
+  i = 0;
+  while (i<array.length) {
+   a = a+array[i];
+ i++;
+  }
+ return a/array.length;  
+}
+
 }
